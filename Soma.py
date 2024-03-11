01edffa518f770d7af31712e0cba4b76d9625228
Douglas Portatil da Silva <133247686+Douglista@users.noreply.github.com>
2024-03-11 09:21:37 -0300

    Update Soma.py

--- Soma.py
+++ Soma.py
@@ -1,7 +1,3 @@
 def soma(num1, num2):
    resultado = num1 + num2
    return resultado
-num1 = int(input("primeiro num: "))
-num2 = int(input("Insira o segundo número: "))
-resultado = soma(num1, num2)
-print(resultado)
