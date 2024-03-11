a922824dec31979d567f215f3d467038f9581697
Douglas Portatil da Silva <133247686+Douglista@users.noreply.github.com>
2024-03-11 09:14:51 -0300

    Função Soma

---
+++ Soma.py
@@ -0,0 +1,7 @@
+def soma(num1, num2):
+   resultado = num1 + num2
+   return resultado
+num1 = int(input("primeiro num: "))
+num2 = int(input("Insira o segundo número: "))
+resultado = soma(num1, num2)
+print(resultado)
